186868bb038e793bdc987b853c3c1f151989d374
MuriloTFROTB <pereiramurilo540@gmail.com>
2024-05-09 21:16:02 -0300

    truco arrumando a logica

--- src/app/components/jogo/jogo.component.ts
+++ src/app/components/jogo/jogo.component.ts
@@ -1,4 +1,4 @@
-import { Component, } from '@angular/core';
+import { Component,DoCheck } from '@angular/core';
 import { BaralhoService } from '../../services/baralho.service';
 import { Carta } from '../../services/carta';
 import { CommonModule } from '@angular/common';
@@ -15,10 +15,19 @@ export class JogoComponent {
   maoJogador1: Carta[] = [];
   maoJogador2: Carta[] = [];
   manilha?: Carta;
-  jogador1Jogou: boolean = false;
   cartaJ1?: Carta;
   cartaJ2?: Carta;
+
+  playerOneWin=true
+  playerTwoWin=false
+  start=true
+  
   resultado: string | null = null;
+
+
+  pontosPlayerOne: number = 0;
+  pontosPlayerTwo: number = 0;
+
 
   constructor(private truco: BaralhoService) {
   }
@@ -37,33 +46,62 @@ export class JogoComponent {
   }
 
   selecionarCartaJ1(carta: Carta, index: number): void {
+   if(!this.start){
+    return
+   }
     this.cartaJ1 = carta;
-    this.maoJogador1.splice(index, 1); // Remover a carta da mão do jogador 1
-    this.jogador1Jogou = true
-    console.log(this.cartaJ1);
+    this.maoJogador1.splice(index, 1);
+    this.start=false
+    this.playerTwoWin=true
   }
 
   selecionarCartaJ2(carta: Carta, index: number): void {
-    if (!this.jogador1Jogou) {
-      return;
+    if(!this.playerTwoWin){
+      return
     }
-
     this.cartaJ2 = carta;
     this.maoJogador2.splice(index, 1);
-    this.jogador1Jogou = false
-    console.log(this.cartaJ2);
-    this.comparaCartas();
+    this.start=true
+    this.playerTwoWin=false
   }
+
+  ngDoCheck(): void {
+
+    if ( this.cartaJ1 && this.cartaJ2) {
+      this.comparaCartas();
+    }
+    
+  }
+  
   comparaCartas() {
+
     if (this.cartaJ1 && this.cartaJ2) {
       if (this.cartaJ1.powerValue > this.cartaJ2.powerValue) {
         this.resultado = 'jogador 1 ganhou';
+        this.pontosPlayerOne++;
+        this.start=true
+        this.pontosPlayerTwo=null!
+
       } else if (this.cartaJ1.powerValue < this.cartaJ2.powerValue) {
         this.resultado = 'jogador 2 ganhou';
+        this.pontosPlayerTwo++;
+        this.playerTwoWin=true
+        this.start=null!
+        
       } else {
         this.resultado = 'empate';
       }
     }
+    this.cartaJ1=null!
+    this.cartaJ2=null!
+    // Verifica se algum jogador atingiu dois pontos
+    if (this.pontosPlayerOne === 2) {
+      this.resultado = 'Player 1 venceu';
+    } else if (this.pontosPlayerTwo === 2) {
+      this.resultado = 'Player 2 venceu';
+    }
+    console.log(this.cartaJ1)
+    console.log(this.pontosPlayerOne)
   }
 
 }
